66d349e147f9254fe293f8ec55130e52210c3ad4
woojoung <ywj981217@naver.com>
2023-04-12 01:17:22 +0900

    reaction velocity game add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 /*eslint-disable */
-import GuGuDan from "./GamesComponents/gugudan";
-import GugudanGame from "./GamesComponents/gugudancopy";
-import WordPlay from "./GamesComponents/wordplay";
-import { useState, useEffect } from "react";
-import Numbaseball from "./GamesComponents/NumberBaseball";
-import { type } from "@testing-library/user-event/dist/type";
-import ResponseCheck from "./GamesComponents/responsegame";
-import Numberbaseball2 from "./GamesComponents/baseballtest";
+// import GuGuDan from "./GamesComponents/gugudan";
+// import GugudanGame from "./GamesComponents/gugudancopy";
+// import WordPlay from "./GamesComponents/wordplay";
+// import { useState, useEffect } from "react";
+// import Numbaseball from "./GamesComponents/NumberBaseball";
+// import { type } from "@testing-library/user-event/dist/type";
+// import ResponseCheck from "./GamesComponents/responsegame";
+// import Numberbaseball2 from "./GamesComponents/baseballtest";
+// import Numbase from "./GamesComponents/숫자야구";
+
+import ResponseCheck from "./GamesComponents/reactionvelocitygame";
 function App() {
   return (
     <div className="App">
@@ -15,26 +18,11 @@ function App() {
       {/* <WordPlay></WordPlay>
       <Clock></Clock> */}
       {/* <Numbaseball></Numbaseball> */}
-      {<Numberbaseball2 />}
+      {/* {<Numberbaseball2 />} */}
       {/* <ResponseCheck></ResponseCheck> */}
+      <ResponseCheck></ResponseCheck>
     </div>
   );
-
-  // function Clock() {
-  //   const [date, setDate] = useState(new Date());
-
-  //   useEffect(() => {
-  //     const timer = setInterval(() => setDate(new Date()), 1000);
-  //     return () => clearInterval(timer);
-  //   }, []);
-
-  //   return (
-  //     <div>
-  //       <p>현재 시간: {date.toLocaleTimeString()}</p>
-  //       <p>현재 날짜: {date.toLocaleDateString()}</p>
-  //     </div>
-  //   );
-  // }
 }
 
 export default App;
--- src/GamesComponents/reactionvelocitygame.js
+++ src/GamesComponents/reactionvelocitygame.js
@@ -1,45 +1,50 @@
-import React, { useState, useRef, useCallback } from "react";
+/* eslint-disable */
+import { wait } from "@testing-library/user-event/dist/utils";
+import React, { useCallback, useRef, useState } from "react";
 
 const ResponseCheck = () => {
+  const [message, setmassage] = useState("클릭해서 시작하세요");
   const [state, setState] = useState("waiting");
-  const [message, setMessage] = useState("클릭해서 시작하세요.");
   const [result, setResult] = useState([]);
   const timeout = useRef(null);
   const startTime = useRef(0);
   const endTime = useRef(0);
 
+  ///
   const onClickScreen = useCallback(() => {
     if (state === "waiting") {
       timeout.current = setTimeout(() => {
         setState("now");
-        setMessage("지금 클릭");
+        setmassage("지금 클릭!");
         startTime.current = new Date();
-      }, Math.floor(Math.random() * 1000) + 2000); // 2초~3초 랜덤
+      }, Math.floor(Math.random() * 1000) + 2000);
       setState("ready");
-      setMessage("초록색이 되면 클릭하세요.");
+      setmassage("초록색이 되면 클릭하세요");
     } else if (state === "ready") {
-      // 성급하게 클릭
+      //성급
       clearTimeout(timeout.current);
       setState("waiting");
-      setMessage("너무 성급하시군요! 초록색이 된 후에 클릭하세요.");
+      setmassage("너무 성급하시군요 초록이 되면 클릭하세요");
     } else if (state === "now") {
-      // 반응속도 체크
+      // 체크 부분
       endTime.current = new Date();
       setState("waiting");
-      setMessage("클릭해서 시작하세요.");
+      setmassage("클릭해서 시작하세요");
       setResult((prevResult) => {
         return [...prevResult, endTime.current - startTime.current];
       });
     }
   }, [state]);
+
   const onReset = useCallback(() => {
     setResult([]);
   }, []);
 
+  // 평균 시간 구하는 함수
   const renderAverage = () => {
     return result.length === 0 ? null : (
       <>
-        <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
+        <div>평균시간 :{result.reduce((a, c) => a + c) / result.length} ms</div>
         <button onClick={onReset}>리셋</button>
       </>
     );
@@ -48,9 +53,11 @@ const ResponseCheck = () => {
   return (
     <>
       <div id="screen" className={state} onClick={onClickScreen}>
+        {renderAverage()}
         {message}
       </div>
-      {renderAverage()}
     </>
   );
 };
+
+export default ResponseCheck;
